b9fabd2eb9a628324a828bbc94f2d1182afa84b6
vfiguer <vfigueroa19972@iesjoanramis.org>
2023-12-13 20:58:04 +0100

    13/12/2023

--- script.js
+++ script.js
@@ -1,27 +1,70 @@
 const contenidor = document.querySelector('.contenidor');
 const lletra = document.querySelectorAll('.fila .letra:not(.ocupat)');
 const contador = document.getElementById('contador');
-const total = document.getElementById('total');
-const peliculaSelect = document.getElementById('pelicula');
+let paraula = document.getElementById("paraula")
+let vidas = 10
+let start = document.getElementById("start")
+let reset = document.getElementById("reset")
+let secreto =document.getElementById("secreto")
+let palabras = ["perro", "conejo", "pneurotorax", "diafragma", "oftalmologia"]
 
+contenidor.classList.add("pointerNone");
+reset.disabled = true;
+reset.hidden = true
+contador.innerText = vidas;
 
-function actualitzaSelecciolletra(){
-    const lletraSeleccionats = document.querySelectorAll('.fila .lletra.seleccionat');
-    
-    const lletraIndex = [...lletraSeleccionats].map(function(lletra) {
-        return [...lletra].indexOf(lletra);
+start.addEventListener('click', (e) => {
+    reset.disabled = false;
+    reset.hidden =false;
+    start.hidden =true;
+    start.disabled = true;
+    contenidor.classList.remove("pointerNone")
+    startgame();
+})
+
+reset.addEventListener('click', (e) => {
+    contenidor.classList.add("pointerNone");
+    reset.disabled = true;
+    reset.hidden =true;
+    start.hidden =false;
+    start.disabled = false;
+    vidas = 10;
+    actualitzaVidas();
+
+    let letras = document.querySelectorAll(".lletra")
+    letras.forEach((lletra) =>{
+        lletra.classList.remove('correcto');
+        lletra.classList.remove('equivocado');
     })
+})
 
-    localStorage.setItem('lletraSeleccionats', JSON.stringify(lletraIndex))
-
-    const contadorlletraSeleccionats = lletraSeleccionats.length;
-    contador.innerText = contadorlletraSeleccionats;
+function actualitzaVidas(){
+    contador.innerText = vidas;
 }
 
 contenidor.addEventListener('click', (e) => {
+    vidas--;
+    actualitzaVidas();
+
+    if(vidas===0){
+        perder()
+    }
+
     if(e.target.classList.contains('lletra')
-    && !e.target.classList.contains('ocupat')){
-    e.target.classList.toggle('seleccionat')
-    actualitzaSelecciolletra();    
+    && !e.target.classList.contains('equivocado')){
+    e.target.classList.toggle('correcto')
+    actualitzaSelecciolletra();
+
+}}
+)
+
+function perder(){
+
 }
-})
+
+function startgame(){
+    let palabra = palabras[Math.floor(Math.random() * palabras.length)]
+    let palabra_encriptada = palabra.replace(/./g, "-");
+    console.log(palabra, palabra_encriptada);
+    secreto.innerText = palabra_encriptada;
+}
